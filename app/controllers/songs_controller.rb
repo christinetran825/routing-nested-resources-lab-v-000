0313acc5c511745ccdbe112468139dd8bf93dba8
Christine Tran <christinetran825@gmail.com>
2017-11-20 22:09:10 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,12 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-     @song = Artist.find(params[:artist_id]).songs
+     @artist = Artist.find(params[:artist_id])
+     if @artist.nil?
+       redirect_to artists_path, alert: "Artist not found"
+     else
+       @songs = @artist.songs
+     end
    else
      @songs = Song.all
    end
