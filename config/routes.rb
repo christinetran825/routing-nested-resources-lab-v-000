ab954e5da5c20f3133e49730404cfddce952d928
Christine Tran <christinetran825@gmail.com>
2017-11-20 21:59:16 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :artists do 
-    resources :songs, only: [:show, :index]
+  resources :artists do
+    resources :songs, only: [:index, :show]
   end
 
-  
+
   resources :songs
 end
